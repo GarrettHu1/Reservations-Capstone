2b6b2672cfd222e2030196b1f816cf20d9e5d030
Garrett Hu <garretthu4@gmail.com>
2022-04-11 12:25:52 -0700

    added reservations service file

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,6 +2,11 @@
  * List handler for reservation resources
  */
 async function list(req, res) {
+  // lists all reservations for a day (from query)
+
+  
+  const day = req.query.date;
+
   res.json({
     data: [],
   });
